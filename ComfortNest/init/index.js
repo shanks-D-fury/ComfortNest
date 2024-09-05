0434fb9f306dac7ab28c66b5a02c516dd1001708
shanks-D-fury <shashanks9190@gmail.com>
2024-09-05 14:05:52 +0530

    CNP : init data added & tested

---
+++ ComfortNest/init/index.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const hotelInfo = require("../models/hotelListing.js");
+
+main()
+	.then(() => {
+		console.log("Connection successful");
+	})
+	.catch((err) => console.log(err));
+
+async function main() {
+	await mongoose.connect("mongodb://127.0.0.1:27017/ComfortNest");
+}
+
+const init = async () => {
+	await hotelInfo.deleteMany({});
+	await hotelInfo.insertMany(initData.data);
+	console.log("Data saved succesfull");
+};
+
+init();

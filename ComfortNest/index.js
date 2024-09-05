3ea0fb48ff3997ce38207d655e96027c117178a1
shanks-D-fury <shashanks9190@gmail.com>
2024-09-05 17:31:52 +0530

    CNP : Index , show route
    converted the standard price to indian format and added indian rupee symbol , added the non-underline sytling

--- ComfortNest/index.js
+++ ComfortNest/index.js
@@ -26,17 +26,28 @@ app.get("/", (req, res) => {
 	res.send("working");
 });
 
-//This is to upload a test element into the data base
-app.get("/testListing", async (req, res) => {
-	let sampleListing = new hotelInfo({
-		title: "New villa",
-		description: "Latest villa in the hole world",
-		price: 1200,
-		country: "India",
-		location: "Mangaluru",
-	});
-	await sampleListing.save();
-	res.send("sucessful savce");
+// //This is to upload a test element into the data base
+// app.get("/testListing", async (req, res) => {
+// 	let sampleListing = new hotelInfo({
+// 		title: "New villa",
+// 		description: "Latest villa in the hole world",
+// 		price: 1200,
+// 		country: "India",
+// 		location: "Mangaluru",
+// 	});
+// 	await sampleListing.save();
+// 	res.send("sucessful savce");
+// });
+
+app.get("/listings", async (req, res) => {
+	const featchedInfo = await hotelInfo.find();
+	res.render("listings/index.ejs", { featchedInfo });
+});
+
+app.get("/listings/:id", async (req, res) => {
+	let { id } = req.params;
+	const listing = await hotelInfo.findById(id);
+	res.render("listings/show.ejs", { listing });
 });
 
 app.listen(8080, () => {

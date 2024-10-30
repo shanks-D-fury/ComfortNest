078e21167313aec837bf6d85913d44dd5d76599d
shanks-D-fury <shashanks9190@gmail.com>
2024-10-30 21:02:11 +0530

    CNP v2 : Ready for Deployment
     small error in mongo_url

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const reviewsRouter = require("./routes/reviews.js");
 const userRouter = require("./routes/users.js");
 const Mongo_url = process.env.ATLAS_MONGO_URL;
 
+// FROM HERE
 const store = MongoStore.create({
 	mongoUrl: Mongo_url,
 	crypto: {
@@ -32,10 +33,10 @@ const store = MongoStore.create({
 
 store.on("error", () => {
 	console.log("Mongo store Error", err);
-});
+}); // TO HERE comment this while working on local machine
 
 const sessionOptions = {
-	store, // comment this line for hosting from the local machine
+	// store, // comment this line for hosting from the local machine
 	secret: process.env.SESSION_SECRET_KEY,
 	resave: false,
 	saveUninitialized: true,
@@ -71,7 +72,7 @@ main()
 	.catch((err) => console.log(err));
 
 async function main() {
-	await mongoose.connect("mongodb://127.0.0.1:27017/ComfortNest");
+	await mongoose.connect(Mongo_url); // use this while working on the local machine process.env.LOCAL_MAC_MONGO_URL
 }
 
 //home route
